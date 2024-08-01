f2625f254d88a02a2a71a17a238d39f79f66cdf9
Matt Clarkson <mtclarkson@yahoo.com>
2024-08-01 23:36:16 +0000

    fixed error

--- models/customers.sql
+++ models/customers.sql
@@ -31,9 +31,9 @@ with
             customers.customer_id,
             customers.first_name,
             customers.last_name,
-            customer_orders.first_order_date,
+            customer_orders.very_first_order_date,
             customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            coalesce(customer_orders.total_number_of_orders, 0) as number_of_orders
         from customers
 
         left join customer_orders using (customer_id)
